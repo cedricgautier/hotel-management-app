9553f744d336feda2a2571ea1d0fe21cec0fabc1
Cedric Gautier <gautiercedric07@gmail.com>
2024-02-16 21:58:20 +0100

    Add more constants

--- src/ced/constants.py
+++ src/ced/constants.py
@@ -9,6 +9,8 @@ client_string = "client"
 email_string = "email"
 nom_string = "nom"
 
+all_rooms_string = "all_rooms"
+no_rooms_available_string = "No rooms available"
 room_string = "room"
 numero_string = "numero"
 type_string = "type"
@@ -23,19 +25,19 @@ date_arrivee_string = "date_arrivee"
 date_depart_string = "date_depart"
 statut_string = "statut"
 
-
+response_string = "response"
 success_string = "success"
 
 success_chambre_add_string = "You have added "
 success_chambre_maj_string = "Chambre mise a jour avec succes."
 success_chambre_delete_string = "Chambre supprimee avec succes."
-success_all_chambrse_delete_string = "Toutes les chambres etaient supprimes."
+success_all_chambres_delete_string = "Toutes les chambres etaient supprimes."
+success_reservation_add_string = "Réservation ajouté avec succès"
 success_reservation_delete_string = "Réservation annulée avec succès"
-
-no_rooms_available_string = {}
-
 message_string = "message"
 error_add_payload = {"error": "unable to add elements to database"}
 error_payload = {"error": "invalid payload"}
+error_get_payload = {"error": "unable to get element"}
+error_add_payload = {"error": "unable to add element"}
 error_modify_payload = {"error": "unable to modify element"}
 error_delete_all_payload = {"error": "unable to delete all elements"}
